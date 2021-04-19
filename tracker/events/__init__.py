7216b0f05a0bc4356dc03b2699fa3451bffbabdc
Price Hiller <philler3138@gmail.com>
2021-04-18 23:45:07 -0500

    Allowed event types to be checked

--- tracker/events/__init__.py
+++ tracker/events/__init__.py
@@ -7,17 +7,17 @@ log = logging.getLogger(__name__)
 
 
 class EventListener:
-    _listening_events: dict[str, list[callable]] = {}
+    _listening_events: dict[str, list[tuple[callable, type]]] = {}
     _loaded_events: dict[str, list] = {}
 
     @classmethod
-    def listen(cls, event: str):
+    def listen(cls, event: str, event_type: type = str):
         def decorator(function):
             if not cls._listening_events.get(event, None):
                 cls._listening_events[event] = []
                 cls._loaded_events[event] = []
             if function.__code__ not in cls._loaded_events[event]:
-                cls._listening_events[event].append(function)
+                cls._listening_events[event].append((function, event_type))
                 cls._loaded_events[event].append(function.__code__)
         return decorator
 
@@ -26,10 +26,13 @@ class EventListener:
         log.info(f"Parsing event: \"{event_name}\": \"{event_content.strip()}\"")
         if events := cls._listening_events.get(event_name):
             tasks = []
-            for event in events:
-                log.info(f"Listener \"{event.__module__}.{event.__name__}\" handling "
-                         f"\"{event_name}: {event_content.strip()}\"")
-                tasks.append(event(event_content))
+            for listener in events:
+                event: callable = listener[0]
+                event_type: type = listener[1]
+                if event_type == type(event_content):
+                    log.info(f"Listener \"{event.__module__}.{event.__name__}\" handling "
+                             f"\"{event_name}: {event_content.strip()}\"")
+                    tasks.append(event(event_content))
             await asyncio.gather(*tasks)
 
 
